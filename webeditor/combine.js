e39768acf14303c1383866358524735a62b4318f
Robert Fletcher <lobatifricha@gmail.com>
2010-02-26 19:18:22 -0800

    getting roark on the web editor

--- webeditor/combine.js
+++ webeditor/combine.js
@@ -3,6 +3,7 @@ window.onload = setUp;
 function setUp() {
   document.getElementById("csstext").onkeydown=handleKeyPress;
   document.getElementById("htmltext").onkeydown=handleKeyPress;
+  document.getElementById("roarktext").onkeydown=roarkTranslate;
 }
 
 function handleKeyPress(e) {
@@ -11,3 +12,20 @@ function handleKeyPress(e) {
   opener.document.getElementById("body").innerHTML = document.getElementById("htmltext").value;
   window.focus();
 }
+
+function roarkTranslate(e) {
+  var myRoark = document.getElementById("roarktext").value;
+  var myStrings = myRoark.split('\n');
+  var myCSS = '';
+  for ( i = 0; i < myStrings.length; i++ ) {
+    phrase = myStrings[i];
+    var myArray = phrase.split(' ');
+    var x = myHash.getItem(myArray[0]);
+    if ( x ) {
+      myCSS += x + ':' + myArray[1] + ';\n';
+    }
+  }
+  document.getElementById("csstext").value = myCSS;
+  handleKeyPress(e);
+}
+
