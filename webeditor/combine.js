54be46af478601cbd8736463fbe6361c5abb00e0
Robert Fletcher <lobatifricha@gmail.com>
2010-03-03 16:12:43 -0800

    adding roark block support

--- webeditor/combine.js
+++ webeditor/combine.js
@@ -17,15 +17,25 @@ function roarkTranslate(e) {
   var myRoark = document.getElementById("roarktext").value;
   var myStrings = myRoark.split('\n');
   var myCSS = '';
+  var blank = true;
   for ( i = 0; i < myStrings.length; i++ ) {
     phrase = myStrings[i];
     var myArray = phrase.split(' ');
     var x = myHash.getItem(myArray[0]);
-    if ( x ) {
+    if ( blank && myArray[0] != "") {
+      myCSS += myArray[0] + ' {\n';
+      blank = false;
+    } else if ( blank ) {
+      myCSS += '\n';
+    } else if ( myArray[0] == "" ) {
+      myCSS += '}\n\n';
+      blank = true;
+    } else if ( x ) {
       myCSS += x + ':' + myArray[1] + ';\n';
+    } else {
+      myCSS += myArray[0] + ':' + myArray[1] + ';\n';
     }
   }
   document.getElementById("csstext").value = myCSS;
   handleKeyPress(e);
 }
-
